697d614baa89e924965644e00291f48ebe50f88a
simon-xie92 <134343915+simon-xie92@users.noreply.github.com>
2023-07-02 16:12:59 -0700

    controller update

--- src/main/java/com/cmpt276/assignment2/controllers/StudentsController.java
+++ src/main/java/com/cmpt276/assignment2/controllers/StudentsController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.autoconfigure.ssl.SslBundleProperties.Key;
+import org.springframework.boot.autoconfigure.ssl.SslBundleProperties.Key;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
